ec6418b7a4d38a0a28bb63c3ac0550df11917627
吴丽娟 <wlj@zjmax.com>
2018-11-29 10:29:18 +0800

    擦拭的城市

--- Pods/Target Support Files/Pods-CommonPods-PushView/Pods-CommonPods-PushView-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CommonPods_PushView : NSObject
-@end
-@implementation PodsDummy_Pods_CommonPods_PushView
-@end
--- Pods/Target Support Files/Pods-PushViewUITests/Pods-PushViewUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PushViewUITests : NSObject
-@end
-@implementation PodsDummy_Pods_PushViewUITests
-@end
